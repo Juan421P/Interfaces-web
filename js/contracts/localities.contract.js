5b9a67bb7f19d799fe62ad478f157da665fdaccc
Ivanya <ivanya@gmail.com>
2025-10-01 16:28:18 -0600

    Matense gordos 9

--- js/contracts/localities.contract.js
+++ js/contracts/localities.contract.js
@@ -1,53 +1,59 @@
-import { makeContract } from "../lib/contract";
-const { types: t} = makeContract({ schema: {}})
+import { Contract } from './../lib/contract.js';
 
-export const LocalitiesContract = makeContract({
-    schema:{
-        localityID: t.string({
-            required: false
-        }),
-        universityID: t.string({
-            required: true
-        }),
-        isMainLocality: t.string({
-            required:true,
-            min: 1,
-            max: 1
-        }),
-        address: t.string({
-            required: true,
-            min: 1,
-            max: 60
-        }),
-        phoneNumber: t.string({
-            required: false,
-            min: 8,
-            max: 25
-        }),
-        universityName: t.string({
-            required: false
-        }),
-    },
-    scopes: {
-        table: [
-            'localityID',
-            'universityName',
-            'isMainLocality',
-            'address',
-            'phoneNumber'
-        ],
-        create: [
-            'universityID',
-            'isMainLocality',
-            'address',
-            'phoneNumber'
-        ],
-        update: [
-            'localityID', 
-            'universityID', 
-            'isMainLocality', 
-            'address', 
-            'phoneNumber'
-        ],
-    },
-});
+export class LocalitiesContract extends Contract {
+    constructor() {
+        super({
+            schema: {
+                localityID: t.string({
+                    required: false
+                }),
+                universityID: t.string({
+                    required: true
+                }),
+                isMainLocality: t.string({
+                    required: true,
+                    min: 1,
+                    max: 1
+                }),
+                address: t.string({
+                    required: true,
+                    min: 1,
+                    max: 60
+                }),
+                phoneNumber: t.string({
+                    required: false,
+                    min: 8,
+                    max: 25
+                }),
+                universityName: t.string({
+                    required: false
+                }),
+            },
+            scopes: {
+                table: [
+                    'localityID',
+                    'universityName',
+                    'isMainLocality',
+                    'address',
+                    'phoneNumber'
+                ],
+                create: [
+                    'universityID',
+                    'isMainLocality',
+                    'address',
+                    'phoneNumber'
+                ],
+                update: [
+                    'localityID',
+                    'universityID',
+                    'isMainLocality',
+                    'address',
+                    'phoneNumber'
+                ],
+                delete: [
+                    'localityID'
+                ]
+            },
+        });
+    }
+}
